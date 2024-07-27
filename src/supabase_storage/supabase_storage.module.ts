98be44dc6b02256c67960bf59c04b53ee36adc30
Ganesh Nayanajith <ganeshnayanajith40@gmail.com>
2024-07-27 14:16:01 +0530

    save file paths

--- src/supabase_storage/supabase_storage.module.ts
+++ src/supabase_storage/supabase_storage.module.ts
@@ -3,8 +3,11 @@ import { SupabaseStorageController } from './supabase_storage.controller';
 import { SupabaseStorageService } from './supabase_storage.service';
 import { SupabaseService } from '../../lib/services/supabase.service';
 import { ConfigService } from '@nestjs/config';
+import { UploadedFileBatchDataEntity } from './uploaded_file_batch_data.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UploadedFileBatchDataEntity])],
   controllers: [SupabaseStorageController],
   providers: [SupabaseStorageService, SupabaseService, ConfigService],
 })
